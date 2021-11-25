85ab4295780c896dd24f91811accda2972026e1e
Jules-RW <j.udahemuka@bboxx.co.uk>
2021-11-26 00:08:35 +0200

    while loop features and book

--- Exercises/Chap VIII_While_Loop/Guessing game.py
+++ Exercises/Chap VIII_While_Loop/Guessing game.py
@@ -2,12 +2,12 @@ from random import randint
 loses = 0
 gain = 0
 
-while loses < 0 or gain >= 200: 
+while loses == 0 or gain == 200: 
     random_number = randint (1,6)
     guess = eval(input('Guess your number: '))
     if guess != random_number:
-        loses = loses - 10
-        print ('You lost $', 10, 'and you are remaining with, ', loses)
+        loses = loses + 10
+        print ('You lost $', 10, 'and you are remaining with, ', 100 - loses)
     elif guess == random_number:
         gain = gain + 9
         print ('You gained $', 9, 'and your current gain is, ', gain)
