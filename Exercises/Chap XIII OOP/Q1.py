5c7f9c54bbce9383c8cb7b0861b25256d3158dae
Jules-RW <j.udahemuka@bboxx.co.uk>
2022-01-24 23:33:44 +0200

    OOP

--- Exercises/Chap XIII OOP/Q1.py
+++ Exercises/Chap XIII OOP/Q1.py
@@ -1,9 +1,12 @@
 
-class Investment ():
+class Investment:
     def __init__(self, p, i, n):
-        p = Self.p
-        i = Self.i 
-        n = Self.n
-    def principal ():
-        
-    def interest ():
+        p = self.p
+        i = self.i 
+        n = self.n
+    def principal (self):
+        return self.p*(1+self.i)**self.n 
+    
+e = Investment (40000000, 0.11, 15)
+
+print (e.principal())
