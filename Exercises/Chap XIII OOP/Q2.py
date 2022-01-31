352dfbac01c61f08fc863ae6f8ac8ebbfc588d38
Jules-RW <j.udahemuka@bboxx.co.uk>
2022-01-31 23:19:36 +0200

    new ideas

--- Exercises/Chap XIII OOP/Q2.py
+++ Exercises/Chap XIII OOP/Q2.py
@@ -1,18 +1,20 @@
 class Product:
-    def __init__ (self, name, amount, price):
-        self.name = name
+    def __init__ (self, amount, price):
+        #self.name = "Product"
         self.amount = amount
         self.price = price
+       
     def get_price (self):
         return self.amount * self.price
+
     def make_purchase (self):
-        if self.amount > 10:
-            return self.get_price 
-        elif self.amount > 100:
-            return self.get_price * 0.1
-        elif self.amount <= 100:
-            return self.get_price * 0.2
+        if self.amount < 10:
+            return self.get_price() 
+        elif self.amount < 100:
+            return self.get_price() * 0.9
+        elif self.amount >= 100:
+            return self.get_price() * 0.8
 
-e = Product (3, 8, 10)
+e = Product (100, 10)
 
-print (e.make_purchase)
+print (e.make_purchase())
