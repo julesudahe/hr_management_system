d9ccfbf804ea1c4612447d59bb3691b813c513b1
Jules-RW <j.udahemuka@bboxx.co.uk>
2022-02-02 00:24:24 +0200

    password

--- Exercises/Chap XIII OOP/Q3 Password_manager.py
+++ Exercises/Chap XIII OOP/Q3 Password_manager.py
@@ -1,2 +1,13 @@
 class Password_manager:
+    def __str__(self, latest_password):
+        self.latest_password = latest_password 
+    def old_password (self):
+        old_password_list = []
+        for i in range (100):
+            old_password_list.append (self.latest_password)
     
+    def get_password (self):
+        return max (self.old_password())
+
+    def set_password (self):
+        
