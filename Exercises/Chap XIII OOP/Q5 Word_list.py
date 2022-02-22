cad2f5eb2f96f6d3c098db9eb79bd939b08cf5fd
Jules-RW <j.udahemuka@bboxx.co.uk>
2022-02-22 23:41:34 +0200

    new things

--- Exercises/Chap XIII OOP/Q5 Word_list.py
+++ Exercises/Chap XIII OOP/Q5 Word_list.py
@@ -6,32 +6,32 @@ class Worldplay:
             words = words.replace (c, '')
         words = words.lower ()
         self.string_word = words.split ()
-    
    
     def word_with_lenght (self):
         word_lenght = []
         for i in range (len (self.string_word)):
-            word_lenght = word_lenght.append (len (self.string_word))
-        print (word_lenght)
+            word_lenght.append (len (self.string_word [i]))
+        print ('Lenght of words in our list is: ', word_lenght)
     
     def start_with_s (self):
         start_with = []
         for i in range (len (self.string_word)):
-            if self.string_word [i] == 's':
-                start_with = start_with.append(len (self.string_word))
-        print (start_with)
+            if self.string_word [0] == 's':
+                start_with.append(len (self.string_word [i]))
+        print ('Words that starts with S: ', start_with)
     
     def end_with_s (self):
         end_with = []
         for i in range (len (self.string_word)):
             if self.string_word [-len (self.string_word) + i] == 's':
-                end_with = end_with.append (len (self.string_word))
-        print (end_with)
+                end_with.append (len (self.string_word))
+        print ('Word that ends with S: ', end_with)
 
-word = input ('Enter your word per word to make your list: ')
+words = input ('Enter your word per word to make your list: ')
 
 
-final_list_word = Worldplay (word)
-print (final_list_word.__init__())
+final_list_word = Worldplay (words)
+print (final_list_word.string_word)
+print (final_list_word.word_with_lenght())
 print (final_list_word.end_with_s ())
 print (final_list_word.start_with_s ())
