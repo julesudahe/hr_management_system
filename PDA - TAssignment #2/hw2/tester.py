24d7b5fb3b9dee730b83ac06309b578a68b59a29
julesudahe <j.udahemuka@bboxx.co.uk>
2023-10-26 02:41:18 +0200

    final changes for today, completed attendance, employee, deserializer, and salary

--- PDA - TAssignment #2/hw2/tester.py
+++ PDA - TAssignment #2/hw2/tester.py
@@ -1,26 +1,13 @@
 """AndrewID: judahemu & Jeannette"""
 
 # Importing different class to be used here during testing
-from employee import Employee
-from manager import Manager
-from director import Director
-from intern import Intern
+from employee_master import Employee
+from salary import Salary
+from attendance import Attendance
+from employee_management import EmployeeManagement
+
 
 employee_example = Employee("Jules", "Udahemuka", "Male", 60000.0)
-manager_example = Manager("Jacky", "Mukakalinda", "Male", 60000.0, "Business Operations", 5, 10)
-director_example = Director("Emmy", "Iraturinze", "Male", 60000.0, 20000.0, "Data Insight", 5)
-intern_example = Intern("Romalice", "Ishimwe", "Male", 60000.0, "Carnegie Mellon University", "MS in Engineering AI", 5)
-
-print('-------------------------------\n')
-
-employee_example.show_employee_info()
-print('-------------------------------\n')
-
-manager_example.show_manager_info()
-print('-------------------------------\n')
-
-director_example.show_director_info()
-print('-------------------------------\n')
-
-intern_example.show_intern_info()
-print('-------------------------------')
+manager_example = Salary("Jacky", "Mukakalinda", "Male", 60000.0, "Business Operations", 5, 10)
+director_example = Attendance("Emmy", "Iraturinze", "Male", 60000.0, 20000.0, "Data Insight", 5)
+intern_example = EmployeeManagement("Romalice", "Ishimwe", "Male", 60000.0, "Carnegie Mellon University", "MS in Engineering AI", 5)
